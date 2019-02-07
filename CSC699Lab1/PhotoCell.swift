e1f1a188191a215942f46bd664ac2ba73c7c1643
Micaella Morales <morales.micaella@gmail.com>
2019-02-07 14:42:20 -0800

    Built the Photo Feed

---
+++ CSC699Lab1/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  CSC699Lab1
+//
+//  Created by Micaella Morales on 2/4/19.
+//  Copyright © 2019 Micaella Morales. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    @IBOutlet weak var tumblrImageView: UIImageView!
+    
+    
+}
--- CSC699Lab1/PhotosViewController.swift
+++ CSC699Lab1/PhotosViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class PhotosViewController: UIViewController {
-
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var posts: [[String: Any]] = []
+    @IBOutlet weak var tumblrTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tumblrTableView.delegate = self
+        tumblrTableView.dataSource = self
         
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
@@ -27,12 +32,41 @@ class PhotosViewController: UIViewController {
                 print(dataDictionary)
                 
                 // TODO: Get the posts and store in posts property
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
                 
                 // TODO: Reload the table view
+                self.tumblrTableView.reloadData()
             }
         }
         task.resume()
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        
+        let post = posts[indexPath.row]
+        if let photos = post["photos"] as? [[String: Any]] {
+            // photos is NOT nil, we can use it!
+            // TODO: Get the photo url
+            // 1.
+            let photo = photos[0]
+            // 2.
+            let originalSize = photo["original_size"] as! [String: Any]
+            // 3.
+            let urlString = originalSize["url"] as! String
+            // 4.
+            let url = URL(string: urlString)
+            cell.tumblrImageView.af_setImage(withURL: url!)
+        }
+        
+        return cell
+    }
+    
 }
